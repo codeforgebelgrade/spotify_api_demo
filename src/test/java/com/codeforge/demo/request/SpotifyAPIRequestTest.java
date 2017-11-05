35de79f4156cf0b27c8e4a541f6a5dea1318a8ed
Miroslav Lazovic <miroslav.lazovich@outlook.com>
2017-11-05 01:21:37 +0100

    Unit test added

--- src/test/java/com/codeforge/demo/request/SpotifyAPIRequestTest.java
+++ src/test/java/com/codeforge/demo/request/SpotifyAPIRequestTest.java
@@ -1,7 +1,18 @@
-package com.codeforge.demo;
+package com.codeforge.demo.request;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
-class ArtistInfoControllerTest {
+import java.io.IOException;
 
+class SpotifyAPIRequestTest {
+
+    @Test
+    public void getArtisInfoTest() throws IOException {
+
+        String responseContent = SpotifyAPIRequestHandler.executeHttpRequest("Nickelback",
+                "Bearer BQASdWGDWXoU6uCy5pVrPwsAZLwczxHaaeaPXA-K8S2iFIiAzy_KzNZFhoym0SeGQRphi28iLbELmWsxJR6ecvKVPIdO7zH076aVZdEpLiridpsbY2GJESblYm4ECUMa2ctPM36IkYRz");
+
+        Assert.assertNotNull(responseContent);
+    }
 }
