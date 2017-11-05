8eb23af211f23b28d44bd9b2f5bfad4afd685f8a
Miroslav Lazovic <miroslav.lazovich@outlook.com>
2017-11-05 01:21:24 +0100

    Added new logic and helper classes

--- src/main/java/com/codeforge/demo/controller/ArtistInfoController.java
+++ src/main/java/com/codeforge/demo/controller/ArtistInfoController.java
@@ -1,11 +1,6 @@
-package com.codeforge.demo;
+package com.codeforge.demo.controller;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import com.codeforge.demo.request.SpotifyAPIRequestHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -20,14 +15,14 @@ public class ArtistInfoController {
 
     @GetMapping
     @RequestMapping(method = RequestMethod.GET)
-    public String getArtistInfo(@RequestParam(value="artistname", required=true) String artistName,
-                                @RequestHeader(value = "Authorization") String auth) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    public String getArtistInfo(@RequestParam(value="artistname", required = true) String artistName,
+                                @RequestHeader(value = "Authorization", required = true) String auth) throws InterruptedException, ExecutionException, TimeoutException {
 
         CompletableFuture<String> responseFuture = CompletableFuture.supplyAsync(() -> {
 
             String response = null;
             try {
-                response = executeHttpRequest(artistName, auth);
+                response = SpotifyAPIRequestHandler.executeHttpRequest(artistName, auth);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -35,27 +30,6 @@ public class ArtistInfoController {
         });
 
         return responseFuture.get(5, TimeUnit.SECONDS);
-    }
-
-    private String executeHttpRequest(String artistName, String auth) throws IOException {
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet getRequest = new HttpGet("https://api.spotify.com/v1/search?q="+ artistName + "&type=artist");
-        getRequest.setHeader("Authorization", auth);
-        CloseableHttpResponse response = client.execute(getRequest);
-        String responseContent = null;
-
-        try {
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                responseContent = EntityUtils.toString(entity);
-            }
-            EntityUtils.consume(entity);
-        } finally {
-            response.close();
-        }
-
-        return responseContent;
     }
 
 
--- src/main/java/com/codeforge/demo/request/SpotifyAPIRequestHandler.java
+++ src/main/java/com/codeforge/demo/request/SpotifyAPIRequestHandler.java
@@ -1,4 +1,36 @@
 package com.codeforge.demo.request;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.platform.commons.annotation.Testable;
+
+import java.io.IOException;
+
+@Testable
 public class SpotifyAPIRequestHandler {
+
+    public static String executeHttpRequest(String artistName, String auth) throws IOException {
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet getRequest = new HttpGet("https://api.spotify.com/v1/search?q="+ artistName + "&type=artist");
+        getRequest.setHeader("Authorization", auth);
+        CloseableHttpResponse response = client.execute(getRequest);
+        String responseContent = null;
+
+        try {
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                responseContent = EntityUtils.toString(entity);
+            }
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return responseContent;
+    }
 }
